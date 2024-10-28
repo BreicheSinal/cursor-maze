f820b13eddc117844824848b5446adef5350dd64
BreicheSinal <sinalbreiche@gmail.com>
2024-10-28 21:32:33 +0200

    Setup functions based on game logic

---
+++ scripts/myTools.js
@@ -0,0 +1,4 @@
+var startGame = document.getElementById('start');
+var endGame = document.getElementById('end');
+var boxDiv = document.getElementById('example');
+var boundaries = document.querySelectorAll('.boundary');
---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+startGame.addEventListener('mouseover', startGame);
+boundaries.addEventListener('mouseover', endGame);
+
+function startGame(){
+}
+
+function endGame(){
+}
+
+function resetGame(){
+}
